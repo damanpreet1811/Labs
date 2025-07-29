cfe011cffe9ca0c448f2de0290b020d2c21daa56
damanpreet1811 <156744553+damanpreet1811@users.noreply.github.com>
2025-07-29 10:00:16 +0530

    Add files via upload

---
+++ ExpenseManager/ExpenseManagerTests/ExpenseManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagerTests.swift
+//  ExpenseManagerTests
+//
+//  Created by Damanpreet Singh on 17/07/25.
+//
+
+import Testing
+@testable import ExpenseManager
+
+struct ExpenseManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
